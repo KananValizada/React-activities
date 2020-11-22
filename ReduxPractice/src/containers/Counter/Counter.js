57b235ee84f1570f395cf844d99d3efb180223f8
KananValizada <kenan.veli@yahoo.com>
2020-11-22 21:35:46 +0300

    Redux delete and store functions

--- ReduxPractice/src/containers/Counter/Counter.js
+++ ReduxPractice/src/containers/Counter/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-
+import * as actions from '../../store/actions'
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -14,7 +14,15 @@ class Counter extends Component {
                 <CounterControl label="Increment" clicked={this.props.onIncremetentCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter}  />
+                <CounterControl label="Subtract 10" clicked={this.props.onSubstractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult} >Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(res=>(
+                         <li key={res.id} onClick={()=>this.props.onDeleteResult(res.id)}>{res.value}</li>
+                    ))}
+                   
+                </ul>
             </div>
         );
     }
@@ -22,16 +30,20 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr:state.counter
+        ctr:state.counter,
+        storedResults:state.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncremetentCounter: () => dispatch({type:'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type:'DECREMENT'}),
-        onAddCounter: () => dispatch({type:'ADD'}),
-        onSubstractCounter: () => dispatch({type:'SUBSTRACT'})
+        onIncremetentCounter: () => dispatch({type:actions.INCREMENT}),
+        onDecrementCounter: () => dispatch({type:actions.DECREMENT}),
+        onAddCounter: () => dispatch({type:actions.ADD,val:5}),
+        onSubstractCounter: () => dispatch({type:actions.SUBSTRACT,val:10}),
+        onStoreResult: () => dispatch({type:actions.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type:actions.DELETE_RESULT,id:id})
+
     }
 }
 
--- ReduxPractice/src/store/reducer.js
+++ ReduxPractice/src/store/reducer.js
@@ -1,28 +1,45 @@
+import * as actions from './actions'
+
 const initialState = {
-    counter:0
+    counter:0,
+    results:[]
 }
 
 const reducer = (state = initialState,action)=>{
 
-    if(action.type === 'INCREMENT'){
-        return {
-            counter:state.counter+1
-        }
-    }
-    if(action.type === 'DECREMENT'){
-        return {
-            counter:state.counter - 1
-        }
-    }
-    if(action.type === 'ADD'){
-        return {
-            counter:state.counter + 5
-        }
-    }
-    if(action.type === 'SUBSTRACT'){
-        return {
-            counter:state.counter - 5
-        }
+    switch(action.type){
+        case actions.INCREMENT:
+             return  {
+                 ...state,
+            counter:state.counter+1 
+            };
+        case actions.DECREMENT:
+            return {
+                ...state,
+                counter:state.counter-1 
+            };
+        case actions.ADD:
+            return {
+                ...state,
+                counter:state.counter + action.val
+            }
+        case actions.SUBSTRACT:
+            return {
+                ...state,
+                counter:state.counter - action.val
+            }
+        case actions.STORE_RESULT:
+            return {
+                ...state,
+                results:state.results.concat({id: new Date() ,value:state.counter})
+            }
+        case actions.DELETE_RESULT:
+            const uptadetArray =state.results.filter(result=>result.id !== action.id) 
+            return {
+                ...state,
+                results:uptadetArray
+            }
+
     }
 
     return state
