79087be1c031cef667d6b26e38832fb31d45a514
KananValizada <kenan.veli@yahoo.com>
2022-01-27 01:27:03 +0400

    update

--- projectv.0.0.2/client/src/App.js
+++ projectv.0.0.2/client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Challanges from "./components/Challanges/Challanges";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Challanges />
     </div>
   );
 }
---
+++ projectv.0.0.2/client/src/components/Challanges/Challanges.js
@@ -0,0 +1,116 @@
+import React from "react";
+import classes from "./Challanges.module.css";
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} ${classes.slickNext}`}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} ${classes.slickPrev}`}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
+
+const Challanges = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  return (
+    <div className={classes.challanges}>
+      <div className={classes.challangesHeader}>
+        <h1>AKTİV TURNİRLƏR</h1>
+        <button>Hamısına bax</button>
+      </div>
+      <Slider {...settings}>
+        <div className={classes.challengeSlideItem}>
+          <div className={classes.challengeSlideImgBack}>
+            <div className={classes.challengeSlideImg}></div>
+          </div>
+          <div className={classes.challengeSlideContent}>
+            <h2>STOL ÜSTÜ TENNİS</h2>
+            <h4>
+              1 Fevral 2020 - 15 Fevral 2020
+              <br />
+              <br />
+              <span>10 AZN</span>
+            </h4>
+            <button>Daha ətraflı</button>
+          </div>
+        </div>
+        <div className={classes.challengeSlideItem}>
+          <div className={classes.challengeSlideImgBack}>
+            <div className={classes.challengeSlideImg2}></div>
+          </div>
+          <div className={classes.challengeSlideContent}>
+            <h2>ŞAHMAT</h2>
+            <h4>
+              1 Fevral 2020 - 07 Fevral 2020
+              <br />
+              <br />
+              <span>5 AZN</span>
+            </h4>
+            <button>Daha ətraflı</button>
+          </div>
+        </div>
+        <div className={classes.challengeSlideItem}>
+          <div className={classes.challengeSlideImgBack}>
+            <div className={classes.challengeSlideImg3}></div>
+          </div>
+          <div className={classes.challengeSlideContent}>
+            <h2>FOTO MÜSABİQƏ</h2>
+            <h4>
+              12 Fevral 2020 - 28 Fevral 2020
+              <br />
+              <br />
+              <span>10 AZN</span>
+            </h4>
+            <button>Daha ətraflı</button>
+          </div>
+        </div>
+        <div className={classes.challengeSlideItem}>
+          <div className={classes.challengeSlideImgBack}>
+            <div className={classes.challengeSlideImg}></div>
+          </div>
+          <div className={classes.challengeSlideContent}>
+            <h2>MOCK</h2>
+            <h4>
+              1 Fevral 2020 - 15.02.2020
+              <br />
+              <br />
+              <span>10 AZN</span>
+            </h4>
+            <button>Daha ətraflı</button>
+          </div>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Challanges;
